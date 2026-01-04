b664ab32ac1ce471a592c75bb2af201b1c014bfc
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2026-01-04 18:53:20 +0100

    feat(admin): implement admin authentication

---
+++ src/utils/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request interceptor to attach token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("admin_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
